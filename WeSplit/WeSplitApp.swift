679fec730b13836bd493909a8a66960e6eaac0c0
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2020-12-18 20:49:01 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jorge Gonzalez on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
